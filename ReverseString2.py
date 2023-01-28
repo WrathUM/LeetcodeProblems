9dc15f8af4e36ad20167a3914319307ae1eaabd4
Walter Guo <57931859+WrathUM@users.noreply.github.com>
2023-01-28 16:33:45 -0500

    Create ReverseString2.py

---
+++ ReverseString2.py
@@ -0,0 +1,13 @@
+def reverseStr(self, s, k):
+      """
+      :type s: str
+      :type k: int
+      :rtype: str
+      """
+      vec = [s[start:start+k] for start in range(0, len(s), k)]
+
+      for i in range(len(vec)):
+          if i % 2 == 0:
+              vec[i] = "".join(reversed(vec[i]))
+
+      return "".join(vec)
