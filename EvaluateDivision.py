7f9d3c483b689273be1a388f47909ad103cc2633
Walter Guo <57931859+WrathUM@users.noreply.github.com>
2023-04-18 23:20:56 -0400

    Update EvaluateDivision.py

--- EvaluateDivision.py
+++ EvaluateDivision.py
@@ -1,24 +1,37 @@
 def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
-        def answer(curr: str, value: float) -> float:
-            # required: self.graph, self.goal, self.seen
-            if curr == self.goal:
-                return value
-            for adj, adj_value in self.graph[curr].items():
-                if adj not in self.seen:
-                    self.seen.add(adj)
-                    result = answer(adj, value*adj_value)  # orig/adj = (orig/curr) * (curr/adj) = value*adj_value
-                    if result != -1:
-                        return result
-                    self.seen.remove(adj)  # backtrack
+        
+        def dfs(cur):
+            if cur not in self.graph or target not in self.graph:
+                return -1
+            if cur == self.goal:
+                return 1
+            
+            if self.goal in self.graph[cur]:
+                return self.graph[cur][self.goal]
+            
+            for node in self.graph[cur]:
+                if node not in self.seen:
+                    self.seen.add(node)
+                    temp = dfs(node)
+                    if temp == -1:
+                        continue
+                    else:
+                        return temp * self.graph[cur][node]
             return -1
+            
         
         self.graph = defaultdict(dict)
-        self.seen = set()  # all visited expressions
-        for (a, b), value in zip(equations, values):
-            self.graph[a][b] = value  # a/b = value, given
-            self.graph[b][a] = 1/value  # b/a = 1/(a/b) = 1/value
-        result = []
-        for orig, self.goal in queries:
-            result.append(answer(orig, 1) if orig in self.graph else -1)
+        self.seen = set()
+        
+        for pair, v in zip(equations, values):
+            c1, c2 = pair
+            self.graph[c1][c2] = v
+            self.graph[c2][c1] = 1/v
+                 
+        res = []
+        for origin, target in queries:
+            self.goal = target
+            res.append(dfs(origin))
             self.seen.clear()
-        return result
+            
+        return res
